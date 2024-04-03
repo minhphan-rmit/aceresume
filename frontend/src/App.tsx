22e25cad32ef7af48fe2169635938bb24f20919f
Nhat Minh Phan <s3978598@rmit.edu.vn>
2024-04-03 21:00:41 +0700

    Folders Structure & Packages Finalization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import { Home } from './_root/pages';
 import RootLayout from './_root/RootLayout';
-import LandingPage from './_root/pages/landing-page/LandingPage';
 
 const App = () => {
   return (
@@ -21,7 +20,6 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route path="/home" element={<Home />} />
-          <Route path='/landing-page' element={<LandingPage />}></Route>
         </Route>
       </Routes>
     </main>
