594a6d70c6db9a1f3120acad9f12a6babef56a2b
madietran <143418748+madietran711@users.noreply.github.com>
2024-04-24 04:33:45 +0700

    auth with ggl route and welcome mail

--- backend/routers/login.py
+++ backend/routers/login.py
@@ -66,3 +66,48 @@ def login(
         return JSONResponse(
             status_code=404, content={"message": "User does not exist."}
         )
+
+
+@router.post(
+    "/google/login", responses={401: {"model": Message}, 404: {"model": Message}}
+)
+def login(
+    email: str = Form(..., description="Username of the user"),
+):
+    # Find the user account in the database
+    account = Constants.USERS.find_one({"email": email})
+
+    if account:
+        # Check if the account is verified
+        if not account.get("verified_at"):
+            return JSONResponse(
+                status_code=401,
+                content={
+                    "message": "Your account is not verified. Please check your email inbox to verify your account."
+                },
+            )
+
+        # Check if the account is activated
+        if not account.get("is_activate", False):
+            return JSONResponse(
+                status_code=401,
+                content={
+                    "message": "Your account has been deactivated. Please contact support."
+                },
+            )
+
+        access_key = unique_string(80)
+        refresh_key = unique_string(100)
+        user_token = UserToken(
+            user_id=str(account["_id"]),
+            access_key=access_key,
+            refresh_key=refresh_key,
+            created_at=datetime.utcnow(),
+            expires_at=datetime.utcnow() + timedelta(hours=24),  # Token expiration time
+        )
+        Constants.USER_TOKENS.insert_one(user_token.dict())
+        return JSONResponse(content={"email": email, "user_id": str(account["_id"])})
+    else:
+        return JSONResponse(
+            status_code=404, content={"message": "User does not exist."}
+        )
